6e293d5390c7b86dcc42e8c9b44db0d8d6c2bc12
huisheng.jin <huisheng.jinge@gmail.com>
2021-03-31 00:17:28 +0800

    fix: storage test fail

--- src/model/storager.js
+++ src/model/storager.js
@@ -1,27 +1,38 @@
 const fs = require('fs')
 const path = require('path')
-const jsonFile = path.resolve(__dirname, './db.json')
+const jsonFile = path.resolve(__dirname, '../config/db.json')
 
 class Storager {
 	constructor() {
+		this.createFileIfNotExist();
 	}
 
 	set(key, value = '') {
-		fs.readFile(jsonFile, (err, data) => {
-			const json = data ? JSON.parse(data) : {}
-			json[key] = value
-			fs.writeFile(jsonFile, JSON.stringify(json), err => {
-				console.log('写入成功')
-			})
-		})
+		const json = this.readDb();
+		json[key] = value;
+		this.saveDb(json);
 	}
 
 	get(key) {
-		let data = fs.readFileSync(jsonFile,'utf-8');
-		const json = data ? JSON.parse(data) : {}
-		console.log(json[key]);
+		const json = this.readDb();
 		return json[key];
 	}
+
+	createFileIfNotExist() {
+		if (!fs.existsSync(jsonFile)) {
+			fs.writeFileSync(jsonFile, JSON.stringify({}))
+		}
+	}
+
+	readDb() {
+		let data = fs.readFileSync(jsonFile, 'utf-8');
+		return JSON.parse(data.toString());
+	}
+
+	saveDb(json) {
+		fs.writeFileSync(jsonFile, JSON.stringify(json));
+	}
+
 }
 
 module.exports = Storager;
--- test/model/storager.test.js
+++ test/model/storager.test.js
@@ -1,10 +1,24 @@
 const Storager = require('../../src/model/storager');
+const fs = require('fs')
+const path = require('path')
 describe('storage', function () {
 	it('should save key-value item and get it success', function () {
 		let storager = new Storager();
 		storager.set('event', '排期接龙');
-		storager.set('event1', '排期接龙');
-		let item = storager.get('event');
-		expect(item).toBe('排期接龙');
+		let value = storager.get('event');
+		expect(value.toString()).toBe('排期接龙');
 	});
+
+	it('should create db.json file when get key first without set ', function () {
+		const jsonFile = path.resolve(__dirname, '../../src/config/db.json')
+		if (fs.existsSync(jsonFile)) {
+			fs.unlinkSync(jsonFile);
+			expect(fs.existsSync(jsonFile)).toBe(false)
+		}
+		let storager = new Storager();
+		let value = storager.get('event');
+		expect(value).toBe(undefined);
+		expect(fs.existsSync(jsonFile)).toBe(true);
+	});
+
 });
