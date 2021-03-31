f349ec68857162a1b1be2603f3351ecae9cf7962
huisheng.jin <huisheng.jinge@gmail.com>
2021-03-31 12:33:13 +0800

    refactor: extract Events class

---
+++ src/model/events.js
@@ -0,0 +1,47 @@
+const Event = require('./event');
+
+class Events {
+	constructor(commingEventDates) {
+		this.events = commingEventDates.map((date) => new Event(date, []));
+	}
+
+	eventsString() {
+		return this.events.map((e, index) => e.toString(index + 1)).join('\n');
+	}
+
+	book(dateOrIndex, name) {
+		const index = Number(dateOrIndex);
+		if (index >= 1 && index <= 5) {
+			this.events[index - 1].appendHost(name);
+			return true;
+		}
+
+		const event = this.events.find((e) => e.date === dateOrIndex);
+		if (event) {
+			event.appendHost(name);
+			return true;
+		}
+
+		return false;
+	}
+
+	travelTo(calendar) {
+		const eventDates = calendar.getComingEventDates(5);
+		this.removeOutdatedEvents(eventDates);
+		this.addComingEvents(eventDates);
+	}
+
+	removeOutdatedEvents(eventDates) {
+		this.events = this.events.filter((e) => eventDates.includes(e.date));
+	}
+
+	addComingEvents(eventDates) {
+		eventDates.forEach((date) => {
+			if (!this.events.find((e) => e.date === date))
+				this.events.push(new Event(date, []));
+		});
+	}
+
+}
+
+module.exports = Events;
--- src/model/schedule.js
+++ src/model/schedule.js
@@ -1,54 +1,27 @@
-const Event = require("./event");
+const Events = require('./events');
+
 class Schedule {
-  constructor(calendar) {
-    const commingEventDates = calendar.getComingEventDates(5);
-    this.events = commingEventDates.map((date) => new Event(date, []));
-  }
+	constructor(calendar) {
+		const commingEventDates = calendar.getComingEventDates(5);
+		this.events = new Events(commingEventDates);
+	}
 
-  toString() {
-    return this.title() + this.eventsString();
-  }
+	toString() {
+		return this.title() + this.events.eventsString();
+	}
 
-  eventsString() {
-    return this.events.map((e, index) => e.toString(index + 1)).join("\n");
-  }
+	title() {
+		return '#排期接龙\n';
+	}
 
-  title() {
-    return "#排期接龙\n";
-  }
+	book(dateOrIndex, name) {
+		return this.events.book(dateOrIndex, name);
+	}
 
-  book(dateOrIndex, name) {
-    const index = Number(dateOrIndex);
-    if (index >= 1 && index <= 5) {
-      this.events[index - 1].appendHost(name);
-      return true;
-    }
+	travelTo(calendar) {
+		this.events.travelTo(calendar);
+	}
 
-    const event = this.events.find((e) => e.date === dateOrIndex);
-    if (event) {
-      event.appendHost(name);
-      return true;
-    }
-
-    return false;
-  }
-
-  travelTo(calendar) {
-    const eventDates = calendar.getComingEventDates(5);
-    this.removeOutdatedEvents(eventDates);
-    this.addComingEvents(eventDates);
-  }
-
-  removeOutdatedEvents(eventDates) {
-    this.events = this.events.filter((e) => eventDates.includes(e.date));
-  }
-
-  addComingEvents(eventDates) {
-    eventDates.forEach((date) => {
-      if (!this.events.find((e) => e.date === date))
-        this.events.push(new Event(date, []));
-    });
-  }
 }
 
 module.exports = Schedule;
