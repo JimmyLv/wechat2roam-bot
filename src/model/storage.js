17e496946e187bae6617637e7aa42cef846ef133
Seaborn Lee <seabornlee.cn@gmail.com>
2021-04-01 07:21:38 +0800

    refactor: move config out of src

--- src/model/storage.js
+++ src/model/storage.js
@@ -1,42 +1,41 @@
-const fs = require('fs')
-const path = require('path')
-const jsonFile = path.resolve(__dirname, '../config/db.json')
+const fs = require("fs");
+const path = require("path");
+const jsonFile = path.resolve(__dirname, "../../config/db.json");
 
 class Storage {
-	constructor() {
-		this.createFileIfNotExist();
-	}
+  constructor() {
+    this.createFileIfNotExist();
+  }
 
-	set(key, value = '') {
-		const json = this.readDb();
-		json[key] = value;
-		this.saveDb(json);
-	}
+  set(key, value = "") {
+    const json = this.readDb();
+    json[key] = value;
+    this.saveDb(json);
+  }
 
-	get(key) {
-		const json = this.readDb();
-		return json[key];
-	}
+  get(key) {
+    const json = this.readDb();
+    return json[key];
+  }
 
-	readDb() {
-		let data = fs.readFileSync(jsonFile, 'utf-8');
-		return JSON.parse(data.toString());
-	}
+  readDb() {
+    let data = fs.readFileSync(jsonFile, "utf-8");
+    return JSON.parse(data.toString());
+  }
 
-	saveDb(json) {
-		fs.writeFileSync(jsonFile, JSON.stringify(json));
-	}
+  saveDb(json) {
+    fs.writeFileSync(jsonFile, JSON.stringify(json));
+  }
 
-	clear() {
-		fs.writeFileSync(jsonFile, JSON.stringify({}))
-	}
+  clear() {
+    fs.writeFileSync(jsonFile, JSON.stringify({}));
+  }
 
-	createFileIfNotExist() {
-		if (!fs.existsSync(jsonFile)) {
-			fs.writeFileSync(jsonFile, JSON.stringify({}))
-		}
-	}
-
+  createFileIfNotExist() {
+    if (!fs.existsSync(jsonFile)) {
+      fs.writeFileSync(jsonFile, JSON.stringify({}));
+    }
+  }
 }
 
 module.exports = Storage;
